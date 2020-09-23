b1dbf8bf3e68d9989f4220c6ed1b8dea6f6b8c77
Jonas Scharpf <jonas@brainelectronics.de>
2020-09-23 18:23:46 +0200

    splitted paths to application and scripts

--- unlock.sh
+++ unlock.sh
@@ -1,5 +1,10 @@
-# define path to scripts and other files
-export OPENOCD_PATH=/usr/share/openocd
+# define path to scripts
+# export OPENOCD_SCRIPTS_PATH=/usr/share/openocd/
+export OPENOCD_SCRIPTS_PATH=/Applications/OpenOCD/
+
+# define path to application
+# export OPENOCD_PATH=/usr/bin/openocd
+export OPENOCD_PATH=${OPENOCD_SCRIPTS_PATH}/bin/
 
 # specify the serial id of the ST Link to use
 # get available ST Link serials via $ st-info --serial
@@ -7,9 +12,9 @@ export ST_LINK_SERIAL_ID=303030303030303030303031
 
 # choose the stlink cfg file accoring to the VID collected by $ lsusb
 # be aware that only the last digit differs for v2 compared to v2-1
-openocd \
-    -f ${OPENOCD_PATH}/scripts/interface/stlink-v2.cfg \
-    -f ${OPENOCD_PATH}/scripts/target/stm32f1x.cfg \
+${OPENOCD_PATH}/openocd \
+    -f ${OPENOCD_SCRIPTS_PATH}/scripts/interface/stlink-v2.cfg \
+    -f ${OPENOCD_SCRIPTS_PATH}/scripts/target/stm32f1x.cfg \
     -c "init" \
     -c "reset halt" \
     -c "stm32f1x unlock 0" \
