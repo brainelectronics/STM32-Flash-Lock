a1a931be96148613182053d860336f1aaf1a5568
Jonas Scharpf <jonas@brainelectronics.de>
2021-05-12 14:33:11 +0200

    use defined path for openocd call, set openocd path as variable without export

--- flash.sh
+++ flash.sh
@@ -1,22 +1,22 @@
 # define path to scripts and other files
-export OPENOCD_PATH=/usr/share/openocd
+OPENOCD_PATH=/usr/share/openocd
 
 # specify the serial id of the ST Link to use
 # get available ST Link serials via $ st-info --serial
-export ST_LINK_SERIAL_ID=303030303030303030303031
+ST_LINK_SERIAL_ID=303030303030303030303031
 
 # define address for flashing
-export FLASH_START_ADDRESS=0x08000000
-export FLASH_LENGTH_STM32F103=0x10000
-export FLASH_BOOTLOADER_OFFSET=0x0000
+FLASH_START_ADDRESS=0x08000000
+FLASH_LENGTH_STM32F103=0x10000
+FLASH_BOOTLOADER_OFFSET=0x0000
 
 # define target file
-export TARGET_FILE=$1
-# export TARGET_FILE=file-name.bin
+TARGET_FILE=$1
+# TARGET_FILE=file-name.bin
 
 # choose the stlink cfg file accoring to the VID collected by $ lsusb
 # be aware that only the last digit differs for v2 compared to v2-1
-openocd \
+${OPENOCD_PATH}/bin/openocd \
     -f ${OPENOCD_PATH}/scripts/interface/stlink-v2.cfg \
     -f ${OPENOCD_PATH}/scripts/target/stm32f1x.cfg \
     -c "program ${TARGET_FILE} ${FLASH_START_ADDRESS} verify reset exit" \
--- unlock.sh
+++ unlock.sh
@@ -1,13 +1,13 @@
 # define path to scripts and other files
-export OPENOCD_PATH=/usr/share/openocd
+OPENOCD_PATH=/usr/share/openocd
 
 # specify the serial id of the ST Link to use
 # get available ST Link serials via $ st-info --serial
-export ST_LINK_SERIAL_ID=303030303030303030303031
+ST_LINK_SERIAL_ID=303030303030303030303031
 
 # choose the stlink cfg file accoring to the VID collected by $ lsusb
 # be aware that only the last digit differs for v2 compared to v2-1
-openocd \
+${OPENOCD_PATH}/bin/openocd \
     -f ${OPENOCD_PATH}/scripts/interface/stlink-v2.cfg \
     -f ${OPENOCD_PATH}/scripts/target/stm32f1x.cfg \
     -c "init" \
